ac384021383f18a5c36ae7d2d77202ad70c36fa4
hannesvdvreken <vandevreken.hannes@gmail.com>
2013-09-13 17:46:41 +0200

    first commit - kruskal

---
+++ src/hannesvdvreken/graph/WeightedGraph.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace hannesvdvreken\graph;
+
+class WeightedGraph
+{
+	private $graph;
+	private $mst; // minimum spanning tree
+
+	/**
+	 * Constructor
+	 * 
+	 * @param array $graph
+	 */
+	public function __construct($graph)
+	{
+		$this->graph = $graph;
+	}
+
+	/**
+	 * Minimum spanning tree (Kruskal Algorithm)
+	 * 
+	 * @return array $mst
+	 */
+	public function mst()
+	{
+		// return cached result
+		if ($this->mst) return $this->mst;
+
+		// init
+		$this->mst = array();
+		$vertices = array();
+
+		// first step: build vertices set
+		foreach ($this->graph as $edge)
+		{
+			$vertices[] = reset($edge['vertices']);
+			$vertices[] = end($edge['vertices']);
+		}
+
+		// make set
+		$vertices = array_unique($vertices);
+		$vertices_used = array();
+
+		// compare function
+		$cmp = function($a, $b) 
+		{
+			return $a['weight'] > $b['weight'];
+		};
+
+		// user defined sort
+		usort($this->graph, $cmp);
+
+		// second step: loop edges, as long as $vertices_u != $vertices
+		if (count($this->graph))
+		{
+			$this->mst[] = $edge = reset($this->graph);
+			$vertices_used = $edge['vertices'];
+		}
+
+		foreach ($this->graph as $edge)
+		{
+			if (in_array(reset($edge['vertices']), $vertices_used) != in_array(end($edge['vertices']), $vertices_used))
+			{
+				// add to mst
+				$this->mst[] = $edge;
+				$vertices_used = array_unique(array_merge($edge['vertices'], $vertices_used));
+			}
+		}
+
+		// report back
+		return $this->mst;
+	}
+}
---
+++ tests/WeightedGraphTest.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use hannesvdvreken\graph\WeightedGraph;
+
+class WeightedGraphTest extends PHPUnit_Framework_TestCase 
+{
+
+	public function testEmpty()
+	{
+		// arrange
+		$graph = array();
+
+		// act
+		$g = new WeightedGraph($graph);
+		$mst = $g->mst();
+
+		// assert
+		$this->assertEquals($mst, array());
+	}
+
+	public function testMst()
+	{
+		// arrange
+		$graph = array(
+			array('vertices' => array('A', 'D'), 'weight' => 5),
+			array('vertices' => array('A', 'B'), 'weight' => 7),
+			array('vertices' => array('B', 'C'), 'weight' => 8),
+			array('vertices' => array('B', 'E'), 'weight' => 7),
+			array('vertices' => array('C', 'E'), 'weight' => 5),
+			array('vertices' => array('D', 'E'), 'weight' => 15),
+			array('vertices' => array('D', 'F'), 'weight' => 6),
+			array('vertices' => array('E', 'F'), 'weight' => 8),
+			array('vertices' => array('E', 'G'), 'weight' => 9),
+			array('vertices' => array('F', 'G'), 'weight' => 11),
+		);
+
+		$mst = array(
+			array('vertices' => array('A', 'D'), 'weight' => 5),
+			array('vertices' => array('D', 'F'), 'weight' => 6),
+			array('vertices' => array('A', 'B'), 'weight' => 7),
+			array('vertices' => array('B', 'E'), 'weight' => 7),
+			array('vertices' => array('B', 'C'), 'weight' => 8),
+			array('vertices' => array('E', 'G'), 'weight' => 9),
+		);
+
+		$g = new WeightedGraph($graph);
+
+		// act
+		$mst = $g->mst();
+
+		// assert
+		$this->assertEquals($g->mst(), $mst);
+	}
+
+}
